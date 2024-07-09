3300f51278fd80ba088ae1ecd9dc7ee861d3a365
Charles Howard <frodare@gmail.com>
2024-07-08 21:46:29 -0400

    Updates

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+importScripts(
+    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+  );
+  
+  workbox.routing.registerRoute(
+    ({ request }) => {
+      return request.destination === 'image' || request.destination === 'audio';
+    },
+    new workbox.strategies.CacheFirst()
+  );
